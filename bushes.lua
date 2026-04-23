fd09d46a72ce9467ffb3e20adf83f761c55b0b58
Tradix <tradixcodes@gmail.com>
2026-04-23 18:42:49 +0300

    O: last update

--- bushes.lua
+++ bushes.lua
@@ -1,12 +1,11 @@
 function updateBushes(dt) end
 
---[[
- function drawBushes()
+
+--[[function drawBushes()
 	for _, bush in ipairs(bushes) do
 		love.graphics.rectangle("line", bush.x, bush.y, bush.w, bush.h)
 	end
 	for _, bush in ipairs(bushes) do
 		bush.anim:draw(sprites.bushSprite, bush.x, bush.y)
 	end
-end
-]]
+end]]

c414680391bf04c9039b77126d66fce552d32b78
Tradix <tradixcodes@gmail.com>
2026-05-15 18:02:11 +0300

    C: minor changes

--- main.lua
+++ main.lua
@@ -1,4 +1,11 @@
-function love.load() end
+-- main file
+function love.load()
+    -- 1. Create the world
+    -- grid, a way to load sprites, a way to load maps, a way to load a camera
+    local sti = require("/library/Simple-Tiled-Implementation/sti")
+    local camera = require("/library/hump/camera")
+    local anim8 = require("/library/anim8/anim8")
+end
 
 function love.update(dt) end
 
---
+++ maps.lua
@@ -0,0 +1,4 @@
+local function initiateGrid()
+    -- we'll load the gamemap variable like mapheight and mapWidth from any given map then we'll use that information to initialise a grid
+    gameMap = sti("levels/ankgor_watt_intro_level")
+end
